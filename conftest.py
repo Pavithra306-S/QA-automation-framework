d4ab245b90d41730a4437cfdf0663f64a2e9d252
Pavithra S <pavithra.s8495@gmail.com>
2026-04-23 16:23:47 +0530

    Added GitHub Actions CI pipeline

--- conftest.py
+++ conftest.py
@@ -8,6 +8,9 @@ from selenium.webdriver.chrome.options import Options
 def driver():
 
     chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     # Disable password manager popup
     chrome_options.add_experimental_option("prefs", {
         "credentials_enable_service": False,
